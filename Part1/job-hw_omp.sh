ab7105599ee720fc4f802c3db999a3cd858024f1
Adam Jacobs <adam.jacobs@login7.ufhpc>
2026-03-02 22:10:03 -0500

    reran c

--- Part1/job-hw_omp.sh
+++ Part1/job-hw_omp.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=classdemo
+#SBATCH --job-name=lab3
 #SBATCH --account=eel6763
 #SBATCH --qos=eel6763
 #SBATCH --nodes=1  #(single node)
 #SBATCH --ntasks=1  #(single process)
-#SBATCH --cpus-per-task=8  #(32 total, class limit)
+#SBATCH --cpus-per-task=16  #(32 total, class limit)
 #SBATCH --mem-per-cpu=600mb
 #SBATCH -t 00:05:00
-#SBATCH -o outfile
+#SBATCH -o Part1_Fig2_16_1600
 #SBATCH -e errfile
-export OMP_NUM_THREADS=4
-./a.out 60 12 10
+export OMP_NUM_THREADS=8
+./a.out 1600 1600 1600
 
